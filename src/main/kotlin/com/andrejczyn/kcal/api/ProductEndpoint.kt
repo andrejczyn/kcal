093c0ed5ae6ca7695593acbcd33691cf778ff0c8
Grzegorz Andrejczyn <grzegorz.andrejczyn@allegro.pl>
2020-10-10 12:47:33 +0200

    return porducts list

--- src/main/kotlin/com/andrejczyn/kcal/api/ProductEndpoint.kt
+++ src/main/kotlin/com/andrejczyn/kcal/api/ProductEndpoint.kt
@@ -1,15 +1,17 @@
 package com.andrejczyn.kcal.api
 
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ProductEndpoint {
+    @CrossOrigin
     @GetMapping("/products", produces = arrayOf("application/json"))
-    fun getProducts() = arrayOf(
+    fun getProducts() = ProductsDto(listOf(
             ProductDto("1", "Marchewka", 200),
             ProductDto("2", "Pomidor", 200)
-    )
+    ))
 }
-
-data class ProductDto(val id: String, val name: String, val kilocalories: Int)
+data class ProductsDto(val products: List<ProductDto>)
+data class ProductDto(val id: String, val name: String, val calories: Int)
