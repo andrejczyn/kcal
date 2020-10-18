0d8e2c1a14aea01e0e4c3176a2d3925e8c9cec24
Grzegorz Andrejczyn <grzegorz.andrejczyn@allegro.pl>
2020-10-18 10:16:02 +0200

    gradle publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,19 +4,17 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.72"
     id("org.springframework.boot") version "2.3.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("pl.allegro.tech.build.axion-release") version "1.12.1"
     id("maven-publish")
     kotlin("plugin.spring") version "1.3.72"
-    // Apply the application plugin to add support for building a CLI application.
     application
 }
 
 group = "com.andrejczyn"
-version = "0.2.0-SNAPSHOT"
+version = scmVersion.version
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-    // Use jcenter for resolving dependencies.
-    // You can declare any Maven/Ivy/file repository here.
     jcenter()
 }
 
@@ -24,8 +22,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    //implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -60,29 +56,20 @@ tasks.withType<KotlinCompile> {
     }
 }
 
-configurations {
-    listOf(configurations.apiElements, configurations.runtimeElements).forEach() {
-        it.get().outgoing.artifacts.removeIf { it.buildDependencies.getDependencies(null).contains(tasks.getByName("jar")) }
-        //it.outgoing.artifact()
-    }
-}
 
 publishing {
     publications {
-        create<MavenPublication>("GitHub") {
-            groupId = "com.andrejczyn"
-            artifactId = "kcal"
-            version = "0.2.0.SNAPSHOT"
-
-            from(components["java"])
+        create<MavenPublication>("DistTar") {
+            artifact(tasks.bootDistTar.get())
         }
     }
     repositories {
         maven {
-//            name = "GitHub"
+            name = "GitHub"
             url = uri("https://maven.pkg.github.com/andrejczyn/kcal")
             credentials {
                 username="andrejczyn"
+                password=System.getenv("GITHUB_PKG")
             }
         }
     }
